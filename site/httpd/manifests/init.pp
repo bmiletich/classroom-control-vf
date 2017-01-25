adaa1be23650bd4dcf021c46d0d095b58f84aafc
psanthoshkumar <psanthoshkumar08@gmail.com>
2017-01-25 13:46:50 -0700

    Update init.pp

--- site/httpd/manifests/init.pp
+++ site/httpd/manifests/init.pp
@@ -1,15 +1,5 @@
 class httpd {
 
-  File { '/etc/httpd/httpd.conf' :
-    ensure => 'file',
-    path => '/etc/httpd/httpd.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    source => 'puppet:///modules/httpd/httpd.conf'
-    require => Package['httpd'],
-    notify  => Service['httpd'],
-  }
   Package { 'httpd' :
     ensure => 'present',
     before => File[httpd.conf]
