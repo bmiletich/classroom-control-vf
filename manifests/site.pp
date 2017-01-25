9072ba229ec80121422edd567952037b92b1bb7b
ccalab1 <ccalab@hotmail.com>
2017-01-25 09:21:04 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -34,7 +34,7 @@ file { '/etc/motd' :
     }
  
 # exec { '/usr/local/bin/cowsay Hi!' : 
-exec { 'cowsay "Welcome to ${::fqdn}" > /etc/motd':
+exec { "cowsay 'Welcome to ${::fqdn}' > /etc/motd":
 #    path => '/bin:/usr/bin/:/usr/local/bin':
  #   creates => '/etc/motd':
     }
