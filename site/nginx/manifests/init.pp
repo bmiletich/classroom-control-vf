8a631b509171862fc0f2e533f2437f52ce17083d
bmiletich <bryan.miletich@gmail.com>
2017-01-25 14:50:21 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,15 +1,29 @@
 class nginx {
   package { 'nginx':
     ensure => present,
-    before => File['nginx.conf']  
+    before => File['nginx.conf'],  
+    before => File['index.html'],
   }
   file { 'nginx.conf':
     ensure => file,
-    path => '/etc/nginx/nginx.conf',
+    path => '/etc/nginx/conf.d/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
     owner => 'root',
     group => 'root',
     notify => Service['nginx.conf'],
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+    path => '/var/www/',
+  }
+  
+  file { 'index.html':
+    ensure => file,
+    path => '/var/www/index.html',
+    owner => 'root',
+    group => 'root',
+    source => 'puppet:///modules/nginx/index.html',
   }
   
   service { 'nginx':
