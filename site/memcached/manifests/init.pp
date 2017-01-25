8a862fb7ab058ed6546065b05959739c1999e36a
abb0tt <stephenabbott20@gmail.com>
2017-01-25 13:27:54 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -7,18 +7,21 @@ class memcached{
     owner => 'root',
     group => 'root',
     mode => '0755',
+    before => File['memcached']
   }
-  file{ '/etc/sysconfig/memcached':
+  
+  file{ 'memcached':
     ensure => file,
     path => '/etc/sysconfig/memcached',
     owner => 'root',
     group => 'root',
     mode => '0644',
     source  => 'puppet:///modules/memcached/memcached',
+    notify => Service['memcached']
+    
   service { 'memcached':
     ensure    => running,
     enable    => true,
-    subscribe => File['/etc/ntp.conf'],
   }
 
 }
