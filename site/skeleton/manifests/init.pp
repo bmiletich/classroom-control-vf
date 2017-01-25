931816c6bb9352b135ca7f5960b8e1c4f6aa99bb
jwjgardner <jgardner@lunagardner.com>
2017-01-25 11:55:40 -0700

    Create init.pp

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,14 @@
+# 9.3 Managin file resources
+class skeleton {
+      file { '/etc/skel':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+      }
+      file { '/etc/skel/.bashrc':
+        ensure => file,
+        owner  => 'root',
+        group  => 'root',
+        source => 'puppet:///modules/skeleton/bashrc',
+      }
+}
