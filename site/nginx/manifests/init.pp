071c6df5367fcc47b0b13a3ef4775e59a64e836d
jwjgardner <jgardner@lunagardner.com>
2017-01-25 14:58:37 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -16,32 +16,32 @@ class nginx {
 		source => 'puppet:///modules/nginx/index.html',
     }
   file { '/etc/nginx/nginx.conf': 
-	  ensure  => file,
-	  owner => ‘root’,
-    group => ‘root’,
-    mode => ‘0664’,
-    source => 'puppet:///modules/nginx/nginx.conf',
-	  require => Package['nginx'], 
-	  notify => Service['nginx'], 
+  	ensure  => file,
+	owner => ‘root’,
+    	group => ‘root’,
+    	mode => ‘0664’,
+    	source => 'puppet:///modules/nginx/nginx.conf',
+    	require => Package['nginx'], 
+    	notify => Service['nginx'], 
    } 
   file { '/etc/nginx/conf.d': 
-	  ensure => directory, 
-	  owner => 'root',
-    group => 'root', 
-	  mode => '0775', 
+	ensure => directory, 
+	owner => 'root',
+    	group => 'root', 
+	mode => '0775', 
   } 
   file { '/etc/nginx/conf.d/default.conf': 
-	  ensure => file,
-	  owner => 'root',
-    group => 'root', 
-	  mode => '0664', 
-	  source => 'puppet:///modules/nginx/default.conf', 
-	  require => Package['nginx'], 
-	  notify => Service['nginx'], 
+	ensure => file,
+	owner => 'root',
+    	group => 'root', 
+	mode => '0664', 
+	source => 'puppet:///modules/nginx/default.conf', 
+	require => Package['nginx'], 
+	notify => Service['nginx'], 
     } 
   service { 'nginx': 
-	  ensure => running, 
-	  enable => true, 
+	ensure => running, 
+	enable => true, 
 	} 
 } 
 
