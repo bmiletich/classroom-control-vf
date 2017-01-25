93af2d6598a344b41258386d12c95fe73951006f
abb0tt <stephenabbott20@gmail.com>
2017-01-25 14:20:22 -0700

    Update init.pp

--- site/nginx/files/manifests/init.pp
+++ site/nginx/files/manifests/init.pp
@@ -2,6 +2,10 @@ class { 'nginx':
   package { 'nginx':
   }
   file { 'documentroot':
+    package => 'nginx',
+    docroot => '/var/www',
+    configfile => '/etc/nginx/nginx.conf',
+    serverblock => '/etc/nginx/conf.d/default.conf'
   }
   file { 'index.html':
   }
