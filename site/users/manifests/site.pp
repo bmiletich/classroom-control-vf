bb7c7ed3478c4c0992d3addfc9797847754c2979
pmedishetty12 <praveenkumar.medishetty@aexp.com>
2017-01-25 10:43:00 -0700

    Create site.pp

---
+++ site/users/manifests/site.pp
@@ -0,0 +1,68 @@
+## site.pp ##
+
+# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
+# used when an agent connects to a master and asks for an updated configuration.
+#
+# Global objects like filebuckets and resource defaults should go in this file,
+# as should the default node definition. (The default node can be omitted
+# if you use the console and don't define any other nodes in site.pp. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.)
+
+## Active Configurations ##
+
+# PRIMARY FILEBUCKET
+# This configures puppet agent and puppet inspect to back up file contents when
+# they run. The Puppet Enterprise console needs this to display file contents
+# and differences.
+
+# Disable filebucket by default for all File resources:
+File { backup => false }
+
+# Randomize enforcement order to help understand relationships
+ini_setting { 'random ordering':
+  ensure  => present,
+  path    => "${settings::confdir}/puppet.conf",
+  section => 'agent',
+  setting => 'ordering',
+  value   => 'title-hash',
+}
+
+# DEFAULT NODE
+# Node definitions in this file are merged with node data from the console. See
+# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
+# node definitions.
+
+# The default node definition matches any node lacking a more specific node
+# definition. If there are no other nodes in this file, classes declared here
+# will be included in every node's catalog, *in addition* to any classes
+# specified in the console for that node.
+
+node default {
+  include role::classroom
+  
+  # example 7.1
+  notify { "Welcome to ${::fqdn}": }
+  
+  file { 'motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/etc/motd',
+    content => "Puppet is fun.\n",
+  }
+
+   # example 7.2
+  exec { "cowsay 'Welcome to ${::fqdn}' > /etc/motd":
+    path    => '/bin:/usr/bin:/usr/local/bin',
+    creates => '/etc/motd',
+  }  
+   # example 7.3
+  host { 'pmedishetty12.puppetlabs.vm':
+    ensure => present,
+    ip     => '127.0.0.1',
+   }
+  
+
+}
