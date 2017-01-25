79b1f1e2ff8d8f6f1390c957a299487c08828d0a
abb0tt <stephenabbott20@gmail.com>
2017-01-25 13:30:56 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -19,7 +19,7 @@ class memcached{
     source  => 'puppet:///modules/memcached/memcached',
     notify => Service['memcached']
     
-  service { 'memcached':
+  service{ 'memcached':
     ensure    => running,
     enable    => true,
   }
