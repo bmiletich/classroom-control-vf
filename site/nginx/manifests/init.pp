7fc24d3598e3ec77794fa29e7015f7ad103c133f
pmedishetty12 <praveenkumar.medishetty@aexp.com>
2017-01-25 15:21:09 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,1 +1,14 @@
-class{"nginx": }
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
+}
