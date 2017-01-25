2df2a72ed7bf80bc0f9b935f61ad7628ffb00262
chennurchaitanya <chaitanya.chennur.us@gmail.com>
2017-01-25 13:17:00 -0700

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,1 @@
+file { '/etc/ntp.conf':  ensure  => file,  owner   => 'root',  group   => 'root',  mode    => '0644',  source  => 'puppet:///modules/ntp/ntp.conf',  require => Package['ntp'],}package { 'ntp':  ensure => present,}service { 'ntpd':  ensure    => running,  enable    => true,  subscribe => File['/etc/ntp.conf'],}
