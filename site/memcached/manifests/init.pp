e7fc5f48aed3b5bb03c20e7d2cac03ea9319e54a
chennurchaitanya <chaitanya.chennur.us@gmail.com>
2017-01-25 13:29:42 -0700

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,20 +1,22 @@
-class memchached {
-file { 'memchached': 
-    ensure  => file, 
-    path  => '/etc/sysconfig/memchached',
-    owner   => 'root',  
-    group   => 'root',  
-    mode    => '0644',  
-    source  => 'puppet:///modules/memchached/memcached',  
-    require => Package['memchached'],
-    notify => Service['memchached'],
-  }
-  package { 'memcached':  
+class memcached {
+  package { 'memcached':
     ensure => present,
-    before => File['memchached'],
+    before => File['memcached'],
   }
-  service { 'memcached':  
-      ensure    => running,
-      enable    => true,
-     }
+  
+  file { 'memcached':
+    ensure => file,
+    path   => '/etc/sysconfig/memcached',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/memcached/memcached',
+    notify => Service['memcached'],
   }
+  
+  service { 'memcached':
+    ensure => running,
+    enable => true
+  }
+
+}
