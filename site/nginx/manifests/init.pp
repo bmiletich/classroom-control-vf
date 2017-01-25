82df67089573cbc6ad2301fa536c9d9886b0a8b6
sp1d3rx <sp1d3rx@gmail.com>
2017-01-25 14:26:06 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -4,7 +4,23 @@ class nginx{
     ensure => present,
   }
 
-  file {'/usr/local/nginx/conf/nginx.conf':
+  file {'/etc/nginx/nginx.conf':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+  
+  file {'/etc/nginx/conf.d':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+  }
+  
+  file {'/etc/nginx/conf.d/default.conf':
     ensure => file,
     owner => 'root',
     group => 'root',
@@ -12,6 +28,27 @@ class nginx{
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
+  
+ file {'/var/www':
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0775',
+  }
+  
+  
+  file {'/var/www/index.html':
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/nginx/index.html',
+    require => Package['nginx'],
+  }
+
+
+
+
 
   service { 'nginx':
     ensure => 'running',
