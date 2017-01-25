df0ca50bd379a16ab3db4ade6b6b1bad3b9e19ad
ccalab1 <ccalab@hotmail.com>
2017-01-25 11:48:38 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -39,9 +39,9 @@ exec { "cowsay 'Welcome to ${::fqdn}' > /etc/motd1":
     creates => '/etc/motd1',
     }
     
-user { 'fundamentals':
-    ensure => present,
-    }
+#user { 'fundamentals':
+ #   ensure => present,
+  #  }
 # DEFAULT NODE
 # Node definitions in this file are merged with node data from the console. See
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
@@ -54,4 +54,5 @@ user { 'fundamentals':
 
 node default {
   include role::classroom
+  include 
 }
