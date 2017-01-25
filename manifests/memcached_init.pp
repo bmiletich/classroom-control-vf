d9410f916cec5a044ad172ecb4707afb6e687926
unixpilot <jeff.devitt1@aexp.com>
2017-01-25 13:37:49 -0700

    Update memcached_init.pp

--- manifests/memcached_init.pp
+++ manifests/memcached_init.pp
@@ -1,1 +1,22 @@
+class memcached {
+
+  package {'memcached':
+  ensure => present,
+  before => File['memcached'],
+  }
+  
+  file { 'memcached':
+    ensure => file,
+    path => 'etc/sysconfig/memcached',
+    owner => 'root',
+    group => 'root',
+    notify => Service['memcached'],
+  }
+  
+  service {'memcached':
+    ensure => running,
+    enable => true,
+    subscribe => File['memcached'],
+    }
+}
 
