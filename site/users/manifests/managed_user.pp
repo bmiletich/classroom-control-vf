10c83d8a5ead0fba845fc67bd617128b52a050ad
rnaraga <rajani.naragam1@aexp.com>
2017-01-26 10:48:11 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,4 +1,13 @@
-class users::managed_user(
- $group = $title,
- ){
- user { $title:
+define users::managed_user (
+  $group = $title,
+) {
+  user { $title:
+    ensure => present,
+  }
+  
+  file { "/home/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+}
