e1a546f29460263fdbeaa860ae9cb87848c154d1
psanthoshkumar <psanthoshkumar08@gmail.com>
2017-01-26 10:50:39 -0700

    Update init.pp

--- site/user/manifests/init.pp
+++ site/user/manifests/init.pp
@@ -2,5 +2,16 @@ class user {
 user { 'fundamental' :
   ensure => 'present',
   }
+class managed_user {
+user { 'fundamental' :
+  home  =>  '/home/fun',
+  owner =>  'fundamental',
+  group =>  'fundamental',
+
 }
+}
+
+}
+
+
 
