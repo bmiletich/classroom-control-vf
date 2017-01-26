ddd1daae864d961e550d14729cbdd32b0169f8c0
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-01-26 07:05:50 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,4 +2,20 @@ class nginx {
   include nginx::packages
   include nginx::config
   include nginx::services
+  
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+  }
+  
+  file { '/var/www':
+    ensure => directory,
+  }
+  
+  file { 'index.html':
+    path   => '/var/www/index.html',
+    source => 'puppet:///modules/nginx/index.html',
+  }
 }
