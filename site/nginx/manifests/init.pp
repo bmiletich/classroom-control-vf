3218b28ae78f40ed4bc150d18f7c65b01752b8c1
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-01-25 18:16:14 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,32 +3,28 @@ class nginx {
     ensure => present,
   }
   
+  File {
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0664',
+  }
+  
   file { '/var/www':
     ensure => directory,
-    owner => 'root',
-    group => 'root',
   }
 
   file { 'index.html':
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
     path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'nginx.conf':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
     path   => '/etc/nginx/nginx.conf',
     source => 'puppet:///modules/nginx/nginx.conf',
   }
 
   file { 'default.conf':
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
     path   => '/etc/nginx/conf.d/default.conf',
     source => 'puppet:///modules/nginx/default.conf',
   }
