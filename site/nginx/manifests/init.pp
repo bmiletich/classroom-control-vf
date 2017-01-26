ab9fb1bc3aefd009a5d9358b7f54f09fe28c5e0f
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-01-25 18:39:06 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,37 +1,5 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
-  }
-  
-  File {
-    ensure => file,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0664',
-  }
-  
-  file { '/var/www':
-    ensure => directory,
-  }
-
-  file { 'index.html':
-    path   => '/var/www/index.html',
-    source => 'puppet:///modules/nginx/index.html',
-  }
-  
-  file { 'nginx.conf':
-    path   => '/etc/nginx/nginx.conf',
-    source => 'puppet:///modules/nginx/nginx.conf',
-  }
-
-  file { 'default.conf':
-    path   => '/etc/nginx/conf.d/default.conf',
-    source => 'puppet:///modules/nginx/default.conf',
-  }
-
-  service { 'nginx':
-    ensure => running,
-    enable => true,
-    subscribe => [ File['nginx.conf'], File['default.conf'] ],
-  }
+  include nginx::packages
+  include nginx::config
+  include nginx::services
 }
