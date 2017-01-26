838973acdfbba71089d18f62d31a0f13d4ac8a6a
chennurchaitanya <chaitanya.chennur.us@gmail.com>
2017-01-26 10:48:26 -0700

    Update managed_user.pp

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -1,1 +1,13 @@
-
+define users::managed_user (
+  $group = $title,
+) {
+  user { $title:
+    ensure => present,
+  }
+  
+  file { "/home/${title}":
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+}
