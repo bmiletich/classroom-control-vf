355fd24ad376d1dc81c8309c3ed8e02d628a1817
abb0tt <stephenabbott20@gmail.com>
2017-01-26 13:12:14 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,13 @@
 class nginx (
-  $root = undef,
-) {
-  case $::osfamily {
-    'redhat','debian' : {
-      $package = 'nginx'
-      $owner = 'root'
-      $group = 'root'
-      #$docroot = '/var/www'
-      $confdir = '/etc/nginx'
-      $logdir = '/var/log/nginx'
-      
-      # this will be used if we don't pass in a value
-      $default_docroot = '/var/www'
-    }
-    default : {
-     fail("Module ${module_name} is not supported on ${::osfamily}")
-    }
-  }
-  
-  # Set the default port for default.conf.epp template
-  $port = '80'
-  # user the service will run as. Used in the nginx.conf.epp template
-  $user = $::osfamily ? {
-    'redhat' => 'nginx',
-    'debian' => 'www-data',
-    'windows' => 'nobody',
-  }
-  
-  # if $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
-  }
-  
+  $package = $nginx::params::package,
+  $owner = $nginx::params::owner,
+  $group = $nginx::params::group,
+  $docroot = $nginx::params::docroot,
+  $confdir = $nginx::params::confdir,
+  $logdir = $nginx::params::logdir,
+  $user = $nginx::params::user,
+  $port = $nginx::params::port
+) inherits nginx::params {
   File {
     owner => $owner,
     group => $group,
@@ -61,10 +36,10 @@ class nginx (
   file { "${confdir}/conf.d/default.conf":
     ensure => file,
     content => epp('nginx/default.conf.epp',
-      {
-        port => $port,
-        docroot => $docroot,
-      }),
+    {
+      port => $port,
+      docroot => $docroot,
+    }),
     notify => Service['nginx'],
   }
   service { 'nginx':
