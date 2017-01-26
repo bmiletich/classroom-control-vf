2d4102c812f48c78975522ec5223bf842cf5cfaf
sohailhussain55 <sohail.hussain@lvcs.co>
2017-01-26 08:02:57 -0700

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,40 @@
+
+class nginx {
+
+  package  { 'nginx':
+    ensure => present,
+  }
+
+  file { '/etc/nginx/':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['openssh'],
+    source  => 'puppet:///modules/ssh/sshd_config',
+  }
+
+
+file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['openssh'],
+    source  => 'puppet:///modules/ssh/sshd_config',
+  }
+
+  service { 'nginx':
+    ensure  => running,
+    enable  => true,
+    require => File['/etc/ssh/sshd_config'],
+  }
+
+}
