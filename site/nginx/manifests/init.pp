f2eb9d9899652cd335b3be2a0acd871c8758da0e
rnaraga <rajani.naragam1@aexp.com>
2017-01-26 08:49:21 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,14 +2,22 @@ class nginx {
   package { 'nginx':
     ensure => present,
   }
+ $docroot = '/var/www'
+  
   File {
     ensure => file,
     owner  => 'root',
     group  => 'root',
     mode   => '0664',
   }
-  file { '/var/www':
+  
+  file { $docroot:
     ensure => directory,
+  }
+  
+  file { 'index.html':
+    path   => "${docroot}/index.html",
+    source => 'puppet:///modules/nginx/index.html',
   }
   
   service { 'nginx':
