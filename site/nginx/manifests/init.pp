829cf454e083023813e6f25703032e12b17857a9
psanthoshkumar <psanthoshkumar08@gmail.com>
2017-01-26 11:50:44 -0700

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,14 +1,38 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
+  include nginx::packages
+  include nginx::config
+  include nginx::services
+  
+  case $::os['family'] {
+    'redhat' : {
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir   = '/var/log/nginx'
+      $owner    = 'root'
+      $group    = 'root'
+    }
+    default : { fail("Unsupported ${module_name} for this ${::os['family']!") }
   }
   
-  file { '/var/www':
+  $user = $::os['family'] ? {
+    'redhat' => 'nginx',
+    default  => 'nginx',
+  }
+  
+  File {
+    ensure => file,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0664',
+  }
+  
+  file { $docroot:
     ensure => directory,
   }
   
-  service { 'nginx':
-    ensure => running,
-    enable => true,
+  file { 'index.html':
+    path   => "${docroot}/index.html",
+    source => 'puppet:///modules/nginx/index.html',
   }
 }
