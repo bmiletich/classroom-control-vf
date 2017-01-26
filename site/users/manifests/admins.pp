4c0313cb5abac5fbcf0e8b1e4a18a2de16d197ca
abb0tt <stephenabbott20@gmail.com>
2017-01-26 10:48:21 -0700

    Update admins.pp

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,15 +1,4 @@
 class users::admins {
-  users::managed_user { 'joe': }
-  
-  users::managed_user { 'alice':
-    group => 'staff',
-  }
-  
-  users::managed_user { 'aaron':
-    group => 'staff',
-  }
-  
-  group { 'staff':
-    ensure => present,
-  }
+  $users = ['alice', 'bob', 'joe' ]
+  users::managed_user { $users: }
 }
